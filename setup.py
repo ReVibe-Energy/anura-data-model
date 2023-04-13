d3ffbaab0f5dddeb9c792d6a6749b4cb37c3c015
Erik Finnman <erik.finnman@layer10.se>
2023-04-13 21:13:07 +0200

    Test with poetry

--- setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_namespace_packages
-
-setup(name="anuradatamodel", version="0.1", packages=find_namespace_packages(where='generated'), package_dir={"": "generated"})
