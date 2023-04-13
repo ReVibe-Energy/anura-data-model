01ae975ed99f32b413c33159e1397791b53e8c2f
Erik Finnman <erik.finnman@layer10.se>
2023-04-13 15:25:36 +0200

    Package and upload

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_namespace_packages
 
-setup(name="anuradatamodel", version="1.0", packages=find_namespace_packages(where='generated'), package_dir={"": "generated"})
+setup(name="anuradatamodel", version="0.1", packages=find_namespace_packages(where='generated'), package_dir={"": "generated"})
