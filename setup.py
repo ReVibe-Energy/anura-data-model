4462e39d243db3b776fd4c4e57ce7cd3290185dc
erikfinnman <erik.finnman@gmail.com>
2023-04-25 13:31:06 +0200

    Build and package (#1)
    * Github actions for model generation
    * Packages built using Poetry
    * Publish to CodeArtifact using twine
    ---------
    
    Co-authored-by: Erik Finnman <erik.finnman@layer10.se>

--- setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_namespace_packages
-
-setup(name="anuradatamodel", version="1.0", packages=find_namespace_packages(where='generated'), package_dir={"": "generated"})
