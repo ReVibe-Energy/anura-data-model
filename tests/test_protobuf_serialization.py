4891c167659cabcb8b93f16f0a65c7bb05e7df5d
Erik Finnman <erik.finnman@layer10.se>
2023-05-04 09:19:32 +0200

    Add some unit tests to demonstrate how nested message behaves

--- tests/test_protobuf_serialization.py
+++ tests/test_protobuf_serialization.py
@@ -1,4 +1,4 @@
-from generated.proto import TheTestMessage
+from generated.proto import TheTestMessage, Node, Root
 
 
 def test_serialization_without_optional():
@@ -36,3 +36,43 @@ def test_serialization_with_default_value():
     deserialized = TheTestMessage().parse(serialized)
     assert deserialized.a_normal_field == 0
     assert deserialized.an_optional_field is None
+
+
+def test_serialization_of_optional_field_with_default_value():
+    message = TheTestMessage(an_optional_field=0)
+    serialized = bytes(message)
+    assert serialized == b'\x10\x00'
+    deserialized = TheTestMessage().parse(serialized)
+    assert deserialized.a_normal_field == 0
+    assert deserialized.an_optional_field is not None
+    assert deserialized.an_optional_field == 0
+
+
+def test_serialization_of_empty_nested_message():
+    root = Root()
+    serialized = bytes(root)
+    assert serialized == b''
+    deserialized = Root().parse(serialized)
+    assert deserialized.node.an_optional_field is None
+    assert deserialized.node is not None
+    assert deserialized.optional_node is None
+
+
+def test_serialization_of_nested_message():
+    node = Node()
+    optional_node = Node()
+    root = Root(node=node, optional_node=optional_node)
+    serialized = bytes(root)
+    assert serialized == b'\n\x00'
+    deserialized = Root().parse(serialized)
+    assert deserialized.node.an_optional_field is None
+    assert deserialized.node is not None
+    assert deserialized.optional_node is not None
+    assert deserialized.optional_node.an_optional_field is None
+
+
+def test_serialization_of_nested_message_with_optional_value():
+    node = Node(an_optional_field=2)
+    root = Root(node=node)
+    serialized = bytes(root)
+    assert serialized == b'\x12\x02\x10\x02'
